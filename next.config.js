e5ce2f6b542186ddad13ca81b096ea59bff1bbfa
Sean Delaney <sean@solspace.com>
2023-05-30 10:19:47 +0100

    Basic demo setup

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/actions/freeform/submit',
+                destination: 'https://www.example.com/actions/freeform/submit',
+            },
+            {
+                source: '/my-module/freeform/form-properties/:formId(\\d{1,})',
+                destination: 'https://www.example.com/my-module/freeform/form-properties/:formId(\\d{1,})',
+            },
+        ];
+    },
+};
+
+module.exports = nextConfig;
